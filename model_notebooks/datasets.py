46a85ceda3bbbf3ce4244ecc57080fd23b2f1a3f
Clem3nti <georgesanders874@gmail.com>
2026-03-06 15:06:51 -0500

    inference and training masking functions implemented but not fully tested. Training pipeline and metrics pipeline currently being implemented

--- model_notebooks/datasets.py
+++ model_notebooks/datasets.py
@@ -1,10 +1,11 @@
 from torch.utils.data import Dataset
 import numpy as np
-
+import torch
 import os
 from tqdm import tqdm
 import random 
 import math 
+import wav2clip
 
 
 PREENCODE_DIR = (
@@ -12,17 +13,21 @@ PREENCODE_DIR = (
 )
 
 
-def apply_mask(dac_encodings, K, u, codebook_size, training): 
+def training_mask(dac_encodings, codebook_size): 
     """
     K layers => K unique masks, 1 per layer
     1. draw from U(0, pi/2)
     2. p = cos(u)
     3. mask ~ bernoulli(p)
     """
+    K = dac_encodings.shape[2]
+
+    u = random.uniform(0, math.pi/2)
+
     shape = dac_encodings.shape
     p = math.cos(u) 
     prob_tensor = torch.full(shape, p)
-    mask_tokens = torch.arange(0, K * codebook_size, 1024).reshape(1, 1, -1)
+    mask_tokens = torch.arange(0, K * codebook_size, codebook_size).reshape(1, 1, -1)
     mask = torch.bernoulli(prob_tensor)
 
     masked_encodings = torch.where(mask == 0, mask_tokens, dac_encodings)
@@ -33,10 +38,87 @@ def apply_mask(dac_encodings, K, u, codebook_size, training):
 
     return masked_encodings, targets
 
-def de_mask(predictions): 
-    pass
+def inference_mask(predictions, cur_step, max_steps): 
+    """
+    predictions : (batch, seq_len, K, codebook_size)
 
-class TrainDataset(Dataset):
+    cos(cur_step/max_steps * math.pi/2)
+    """
+    _, _, K, codebook_size = predictions.shape
+    c = (cur_step + 1) / max_steps
+    cur_percentage = 1 - math.cos(c * math.pi / 2)
+
+    # (batch, seq_len, K)
+    tokens = torch.argmax(predictions, dim=3)
+    confidences = torch.max(predictions, dim=3)
+    mask_tokens = torch.arange(0, K * codebook_size, codebook_size).reshape(1, 1, -1)
+
+    # (batch, seq_len * K)
+    flat_confidences = torch.flatten(confidences, start_dim=1)
+    k = math.ceil(flat_confidences.shape[1] * cur_percentage)
+    _, top_k_indices = torch.topk(flat_confidences, k, dim=1)
+    
+    mask = torch.zeros(flat_confidences.shape)
+    mask.scatter_(1, top_k_indices, 1).reshape(tokens.shape)
+
+    unmasked = torch.where(mask == 1, tokens, mask_tokens)
+    
+    return unmasked
+
+class Metrics(): 
+    """
+    Fréchet distance
+        -> FAD : VGGIsh embedding
+        -> FDM : MFCC
+        -> FDD : DAC
+    Cosine Similarity
+        -> target vs generated
+    Semantic Matching: 
+        -> Wav2Clip 
+        -> embed target embed generated then cosine similarity
+        -> embed generated and compare against original CLIP embddings
+    Alignment of generated audio:
+        -> novelity score: pearson coefficient between BEATS(target) and BEATS(generated)
+        -> SparseSync: mean of offset (not used)
+    """
+
+    """
+    frechet distance VGGish
+    """
+    def FAD(): 
+        pass
+
+    """
+    frechet distance MFCC
+    """
+    def FDM(): 
+        pass
+
+    """
+    frechet distance DAC
+    """
+    def FDD(): 
+        pass
+
+    def wav_cos_sim(): 
+        pass
+
+    def wave_clip(prediction, target):
+
+        wav2clip_model = wav2clip.get_model()
+        emb_preds 
+        
+
+    def cycle_clip(): 
+        pass
+
+    def novelty_score(): 
+        pass
+
+    def get_metrics(predictions, targets): 
+        pass
+
+class AudioVideoDataset(Dataset):
     def __init__(self, root, with_beats=False):
         self.data_paths = []
         self.with_beats = with_beats
@@ -76,36 +158,8 @@ class TrainDataset(Dataset):
         return ret_list
 
 
-class ValidationDataset(Dataset):
-    def __init__(self, root):
-        self.data_paths = []
-
-        dirs = os.listdir(root)
-
-        for dir in dirs:
-            audio_encode_pth = os.path.join(root, dir, "audio_encode")
-
-            if not os.path.exists(audio_encode_pth):
-                continue
-            names = os.listdir(audio_encode_pth)
-
-            for name in tqdm(names):
-                audio_file_path = os.path.join(audio_encode_pth, name)
-
-                self.data_paths.append(audio_file_path)
-
-    def __len__(self):
-        return len(self.data_paths)
-
-    def __getitem__(self, index):
-        audio_file_path = self.data_paths[index]
-        audio_encoding = np.load(audio_file_path)
-
-        return audio_encoding["dac"]
-
-
 def main():
-    audio_encodings = TrainDataset(PREENCODE_DIR)
+    audio_encodings = AudioVideoDataset(PREENCODE_DIR)
 
     dac, beats = audio_encodings.__getitem__(0)
     print(dac.shape, beats.shape)
